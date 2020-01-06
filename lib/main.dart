e16661f11bfe72950ec85c7338821356509b26fd
Julien L <julien.laurencin@gmail.com>
2020-01-06 15:31:54 +0100

    header v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -7,7 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mamie',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,92 +23,198 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      //home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TextAndIconButton(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class TextAndIconButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Mamie'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+      body: new ListView(
+
+        children: <Widget>[
+
+          //You can add more widget bellow
+
+          threeButtonsSection ,
+
+
+
+
+
+        ],
+
+      )
+
+//      body: Center(
+//        child: FlatButton.icon(
+//          color: Colors.blue,
+//          icon: Icon(Icons.settings), //`Icon` to display
+//          label: Text('Settings'), //`Text` to display
+//          onPressed: () {
+//            //Code to execute when Floating Action Button is clicked
+//            //...
+//          },
+//        ),
+//      ),
     );
   }
 }
+
+Widget threeButtonsSection = new Container(
+  child: new Row(
+    mainAxisAlignment: MainAxisAlignment.spaceAround,
+    children: <Widget>[
+      //build in a separated function1A
+      FlatButton.icon(
+        color: Colors.blue,
+        icon: Icon(Icons.settings), //`Icon` to display
+        label: Text('Settings'), //`Text` to display
+        onPressed: () {
+          //Code to execute when Floating Action Button is clicked
+          //...
+        },
+      ),
+
+      FlatButton.icon(
+        color: Colors.blue,
+        icon: Icon(Icons.accessibility_new), //`Icon` to display
+        label: Text('Famille'), //`Text` to display
+        onPressed: () {
+
+
+
+          //Code to execute when Floating Action Button is clicked
+          //...
+        },
+      ),
+
+      FlatButton.icon(
+        color: Colors.blue,
+        icon: Icon(Icons.message), //`Icon` to display
+        label: Text('Message'), //`Text` to display
+        onPressed: () {
+          //Code to execute when Floating Action Button is clicked
+          //...
+        },
+      ),
+    ],
+  ),
+);
+
+
+
+//class MyApp extends StatelessWidget {
+//  //Stateless = immutable = cannot change object's properties
+//  //Every UI components are widgets
+//  @override
+//  Widget build(BuildContext context) {
+//    //Now we need multiple widgets into a parent = "Container" widget
+//    Widget titleSection = new Container(
+//      padding: const EdgeInsets.all(10.0),//Top, Right, Bottom, Left
+//      child: new Row(
+//        children: <Widget>[
+//          new Expanded(
+//            child: new Column(
+//              crossAxisAlignment: CrossAxisAlignment.start,
+//              children: <Widget>[
+//
+//              ],
+//            ),
+//          ),
+//          //new Icon(Icons.favorite, color: Colors.red),
+//          //new Text(" 100", style: new TextStyle(fontSize: 16.0),),
+//        ],
+//      ),
+//    );
+//
+//
+//    Widget buildButton(IconData icon, String buttonTitle) {
+//      final Color tintColor = Colors.blue;
+//      return new Column(
+//        children: <Widget>[
+//          new Icon(icon, color: tintColor),
+//          new Container(
+//            margin: const EdgeInsets.only(top: 5.0),
+//            child: new Text(buttonTitle, style: new TextStyle(fontSize: 16.0,
+//                fontWeight: FontWeight.w600, color: tintColor),),
+//          )
+//        ],
+//      );
+//    }
+//    Widget threeButtonsSection = new Container(
+//      child: new Row(
+//        mainAxisAlignment: MainAxisAlignment.spaceAround,
+//        children: <Widget>[
+//          //build in a separated function1A
+//          buildButton(Icons.settings, "Settings"),
+//          buildButton(Icons.accessibility_new, "Famille"),
+//          buildButton(Icons.message, "Message"),
+//        ],
+//      ),
+//    );
+//
+//    Widget listFamille = new ListView.builder(
+//        itemBuilder: (context, index) {
+//          return Text("$index");
+//        },
+//    );
+//
+//    //build function returns a "Widget"
+//    return new MaterialApp(
+//        title: "",
+//        home: new Scaffold(
+//            appBar: new AppBar(
+//              title: new Text('Flutter App'),
+//            ),
+//
+//
+//            //body: listUser()
+//            body: new ListView(
+//
+//              children: <Widget>[
+//
+//                //You can add more widget bellow
+//
+//                threeButtonsSection ,
+//
+//
+//
+//
+//
+//              ],
+//
+//            )
+//
+//        )
+//    );//Widget with "Material design"
+//  }
+//}
+//
+//class listUser extends StatelessWidget{
+//  @override
+//  Widget build(BuildContext context) {
+//  final users = ['U1','U2', 'U3','U4','U5','U6','U7'];
+//
+//    // TODO: implement build
+//
+//
+//    return ListView.builder
+//      (
+//      itemCount: users.length,
+//
+//      itemBuilder: (context, index) {
+//        return ListTile(
+//          title: new Text(users[index]),
+//        );
+//      },
+//    );
+//  }
+//}
